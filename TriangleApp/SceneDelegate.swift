bfca551023f51f3bef21245f4ff3b0f4980b7f92
nuradityo <nuradityo@gmail.com>
2024-03-29 21:32:38 +0700

    test: add tdd to test triangle creation .yaml file configuration assisted by AI

---
+++ TriangleApp/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  TriangleApp
+//
+//  Created by Admin on 14/03/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+  
+}
+
